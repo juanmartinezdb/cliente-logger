c691a339205781625587ccda8ffc4f22254b9b68
juanmartinezdb <juanmmcontacto@gmail.com>
2025-02-10 09:31:06 +0100

    bla

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,12 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { RouterLink, RouterModule} from '@angular/router';
+import { routes } from '../app.routes';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [],
+  imports: [CommonModule, RouterModule, RouterLink],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.css'
 })
